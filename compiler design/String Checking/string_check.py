9bbc92624a5b274f5b41fd9ad459c3ce9e6ad0a1
Ritwik Bahukhandi <ritwikbahukhandi98@gmail.com>
2021-06-16 23:27:44 +0530

    Add files via upload

---
+++ compiler design/String Checking/string_check.py
@@ -0,0 +1,14 @@
+s = input("Enter a string : ")
+flag = 1
+a="a"
+
+for i in range(0,len(s)-1):
+        flag = 0
+        break
+    if s[i] != a:
+
+if flag==1:
+    print("String belong to the given grammer aa* \n")
+else:
+    print("String doesn't belong to the given grammer aa* \n")
+
